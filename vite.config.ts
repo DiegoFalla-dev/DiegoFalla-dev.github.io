a6bac796839a4aaf4f811214f6d7bf0a4334096d
yosriel <diego.falla.2004.10@gmail.com>
2025-12-21 23:05:46 -0500

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/DiegoFalla-dev.github.io/',
-  plugins: [react()],
-})
